d96e4eeb7fea70a14e03675adb9d9071f8095698
kevindoni <kevindoni17@gmail.com>
2022-01-19 20:56:39 +0700

    Update ohp.sh

--- ohp/ohp.sh
+++ ohp/ohp.sh
@@ -4,7 +4,7 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- ipinfo.io/ip);
-IZIN=$( curl https://raw.githubusercontent.com/SSHSEDANG4/kunci_script/main/iptext.sh | grep $MYIP )
+IZIN=$( curl https://raw.githubusercontent.com/kevindoni/Websocket-cloudfront-ssh/main/sungkem | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 clear
 echo -e "${green} Please Wait, Proses...${NC}"
